bb53e8a1f23952c2e54b265c8f2a912d73b2dd4a
bjfisher <bjfisher1@gmail.com>
2019-02-15 05:25:27 +1100

    added table code

--- js/app.js
+++ js/app.js
@@ -6,6 +6,44 @@ $(document).ready(function() {
 	    afterChange: null, // Callback which is called after views change
   });
 
+  function maketable(path, divcontainer) {
+
+    $.get(path, function(data) {
+
+      // start the table
+      var html = '<table">';
+
+      // split into lines
+      var rows = data.split("\n");
+
+      // parse lines
+      rows.forEach( function getvalues(ourrow) {
+
+        // start a table row
+        html += "<tr>";
+
+        // split line into columns
+        var columns = ourrow.split(",");
+
+        html += "<td>" + columns[0] + "</td>";
+        html += "<td>" + columns[1] + "</td>";
+        html += "<td>" + columns[2] + "</td>";
+
+        // close row
+        html += "</tr>";
+      })
+      // close table
+      html += "</table>";
+
+      // insert into div
+      $(divcontainer).append(html);
+
+    });
+
+  };
+
+  maketable('../csv/cpu.csv', 'div-cpu');
+
   $('.box').click(function() {
     $.fn.viewScroller.showView('thirdview');
   });
